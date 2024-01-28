c91458b7f011e60875db8b096736d0d004c5a4c0
Md. Nishat Raihan Khan Nijhum <mdnishatraihankhan@gmail.com>
2024-01-28 23:30:32 +0600

    upload evenOdd.java

---
+++ Semester 02/Object Oriented Programming 1 (Java)/[0] 28 Jan, 2024/evenOdd.java
@@ -0,0 +1,12 @@
+public class evenOdd {
+   public static void main(String []args) {
+				
+		System.out.println("Even Numbers : ");
+		
+		for(int i = 1; i <= 100; i++)
+		{
+			if(i % 2 == 0)
+				System.out.print(i +" ");
+		}
+   }
+}
