f4d4d8ed09426cc95c29284a5698add7907b877f
Md. Nishat Raihan Khan Nijhum <mdnishatraihankhan@gmail.com>
2024-02-25 08:01:23 +0600

    Create main.java

---
+++ Semester 02/Object Oriented Programming 1 (Java)/[4] 18 Feb, 2024/LabTask01/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setName("Md. Nishat Raihan Khan Nijhum");
+        s1.setId(23532083);
+        s1.setCgpa(3.68);
+        s1.showInfo();
+    }
+}
