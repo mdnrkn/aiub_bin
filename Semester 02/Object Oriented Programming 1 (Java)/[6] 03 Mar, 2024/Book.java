040420546294873fd50f0be24e3a9318ebf406b0
Md. Nishat Raihan Khan Nijhum <mdnishatraihankhan@gmail.com>
2024-03-05 10:16:54 +0600

    Create Book.java

---
+++ Semester 02/Object Oriented Programming 1 (Java)/[6] 03 Mar, 2024/Book.java
@@ -0,0 +1,77 @@
+public class Book
+{
+	private String isbn;
+	private String bookTitle;
+	private String authorName;
+	private double price;
+	private int availableQuantity;
+	
+    public Book( ){}
+    
+	public Book(String isbn, String bookTitle, String authorName, double price, int availableQuantity)
+	{
+		this.isbn=isbn;
+		this.bookTitle=bookTitle;
+		this.authorName=authorName;
+		this.price=price;
+		this.availableQuantity=availableQuantity;
+	}
+	
+	public void setIsbn(String isbn)
+	{
+		this.isbn=isbn;
+	}
+	public void setBookTitle(String bookTitle)
+	{
+		this.bookTitle=bookTitle;
+	}
+	public void setAuthorName(String authorName)
+	{
+		this.authorName=authorName;
+	}
+	public void setPrice(double price)
+	{
+		this.price=price;
+	}
+	public void setAvaiableQuantity(int availableQuantity)
+	{
+		this.availableQuantity=availableQuantity;
+	}
+	public String getIsbn( )
+	{
+		return isbn;
+	}
+	public String getBookTitle( )
+	{
+		return bookTitle;
+	}
+	public String getAuthorName( )
+	{
+		return authorName;
+	}
+	public double getPrice( )
+	{
+		return price;
+	}
+	public int getAvailableQuantity( )
+	{
+		return availableQuantity;
+	}
+	public void addQuantity(int amount)
+	{
+		availableQuantity+=amount;
+	}
+	public void sellQuantity(int amount)
+	{
+		availableQuantity-=amount;
+	}
+	
+	public void showDetails( )
+	{
+		System.out.println("ISBN     : "+isbn);
+		System.out.println("Title    : "+bookTitle);
+		System.out.println("Author   : "+authorName);
+		System.out.println("Price    : "+price);
+		System.out.println("Quantity : "+availableQuantity);
+    }
+}
