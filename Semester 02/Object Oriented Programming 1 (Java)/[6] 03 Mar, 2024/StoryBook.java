5212e7ac6eb568144fd7b11bee9d8aee2b886e3f
Md. Nishat Raihan Khan Nijhum <mdnishatraihankhan@gmail.com>
2024-03-05 10:16:58 +0600

    Create StoryBook.java

---
+++ Semester 02/Object Oriented Programming 1 (Java)/[6] 03 Mar, 2024/StoryBook.java
@@ -0,0 +1,25 @@
+public class StoryBook extends Book 
+{
+    private String category;
+    public StoryBook( ){}
+    public StoryBook(String isbn, String bookTitle, String authorName, double price, int availableQuantity, String category)
+    {
+        super(isbn, bookTitle, authorName,  price, availableQuantity);
+        this.category = category;
+    }
+    
+    public void setCategory(String category)
+    {
+        this.category = category;
+    }
+    public String getCategory( )
+    {
+        return category;
+    }
+
+    public void showDetails()
+    {
+        super.showDetails();
+        System.out.println("Category : "+category);
+    }
+}
