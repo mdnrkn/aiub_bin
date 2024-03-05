38086de0fe0bdaf150da9de0619192edcf8098cd
Md. Nishat Raihan Khan Nijhum <mdnishatraihankhan@gmail.com>
2024-03-05 10:17:02 +0600

    Create TextBook.java

---
+++ Semester 02/Object Oriented Programming 1 (Java)/[6] 03 Mar, 2024/TextBook.java
@@ -0,0 +1,25 @@
+public class TextBook extends Book 
+{
+    private int standard;
+    public TextBook( ){}
+    public TextBook(String isbn, String bookTitle, String authorName, double price, int availableQuantity, int standard)
+    {
+        super(isbn, bookTitle, authorName,  price, availableQuantity);
+        this.standard = standard;
+    }
+    
+    public void setStandard(int standard)
+    {
+        this.standard = standard;
+    }
+    public int getStandard( )
+    {
+        return standard;
+    }
+    
+    public void showDetails()
+    {
+        super.showDetails();
+        System.out.println("Standard : "+standard);
+    }
+}
