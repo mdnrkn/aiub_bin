327925dfe1b3abd9318889c51b587164b0463595
Md. Nishat Raihan Khan Nijhum <mdnishatraihankhan@gmail.com>
2024-03-31 07:45:23 +0600

    Create Book.java

---
+++ Semester 02/Object Oriented Programming 1 (Java)/final/[0] 23 Mar, 2024/Library-Book/Book.java
@@ -0,0 +1,39 @@
+// 2 
+public class Book {
+	private String bookName;
+	private String bookAuthor;
+	private String bookId;
+	private String bookType;
+	private int bookCopy;
+
+	static int bookCounter;
+
+	Book() {};
+
+	Book(String bookName, String bookAuthor, String bookId, String bookType, int bookCopy) {
+		this.bookName = bookName;
+		this.bookAuthor = bookAuthor;
+		this.bookId = bookId;
+		this.bookType = bookType;
+		this.bookCopy = bookCopy;
+		Contact.bookCounter++;
+	}
+
+	public void showInfo() {
+		System.out.println("========= Book Info =========");
+		System.out.println("Book Name: " + this.bookName);
+		System.out.println("Book Author: " + this.bookAuthor);
+		System.out.println("Book Id: " + this.bookId);
+		System.out.println("Book Type: " + this.bookType);
+		System.out.println("Book Copy: " + this.bookCopy);
+	    System.out.println("==============================");
+	}
+
+	public void addBookCopy(int x) {
+		this.bookCopy = this.bookCopy + x;
+	}
+
+	static void showTotalBookInfo() {
+		System.out.println("Total Number of book Object: " + bookCounter);
+	}
+}
