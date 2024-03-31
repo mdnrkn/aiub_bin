d7efeca4fd7c30860c7295e009d8d679b775fbbb
Md. Nishat Raihan Khan Nijhum <mdnishatraihankhan@gmail.com>
2024-03-31 10:44:39 +0600

    Update Run.java

--- [1] Object Oriented Programming 1 (Java)/final/[0] 23 Mar, 2024/Library-Book/Run.java
+++ [1] Object Oriented Programming 1 (Java)/final/[0] 23 Mar, 2024/Library-Book/Run.java
@@ -1,9 +1,9 @@
 public class Run {
   public static void main(String args[]) {
-    AddressBook L = new AddressBook("Tokyo Public Library", "Japan");
+    Library L = new Library("Tokyo Public Library", "Japan");
 
-    Contact B1 = new Contact("Law", "Oscar", "7362855", "Theory", 15);
-    Contact B2 = new Contact("journalism", "Emmett, Miles", "928337747", "Theory", 22);
+    Book B1 = new Book("Law", "Oscar", "7362855", "Theory", 15);
+    Book B2 = new Book("journalism", "Emmett, Miles", "928337747", "Theory", 22);
 
     L.addNewBook(B1);
     L.addNewBook(B2);
