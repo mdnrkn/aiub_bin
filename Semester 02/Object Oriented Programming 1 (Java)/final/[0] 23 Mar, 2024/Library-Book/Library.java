c66022299c1229896f29ef8b644089da54c4b0da
Md. Nishat Raihan Khan Nijhum <mdnishatraihankhan@gmail.com>
2024-03-31 07:45:28 +0600

    Create Library.java

---
+++ Semester 02/Object Oriented Programming 1 (Java)/final/[0] 23 Mar, 2024/Library-Book/Library.java
@@ -0,0 +1,37 @@
+// 5
+public class Library {
+  private String libName;
+  private String libAddress;
+  private Contact[] books = new Contact[9000];
+  private int totalBook;
+
+  Library() {
+  }
+
+  Library(String libName, String libAddress) {
+    this.libName = libName;
+    this.libAddress = libAddress;
+  }
+
+  public void showLibInfo() {
+    System.out.println("========= Library Info =========");
+    System.out.println("Library Name: " + this.libName);
+    System.out.println("Library Address: " + this.libAddress);
+    System.out.println("==============================");
+
+    for (int i = 0; i < this.books.length; i++) {
+      if (this.books[i] != null) {
+        this.books[i].showInfo();
+      }
+    }
+  }
+
+  public void addNewBook(Contact book) {
+    books[totalBook] = book;
+    totalBook++;
+  }
+
+  public void addNewBookCopy(Contact book, int copy) {
+    book.addBookCopy(copy);
+  }
+}
