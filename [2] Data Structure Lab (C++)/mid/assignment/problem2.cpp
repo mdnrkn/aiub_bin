959e984431d508558c3bfcf6dbde9fcf49e8dd69
Md. Nishat Raihan Khan Nijhum <mdnishatraihankhan@gmail.com>
2024-11-28 01:08:00 +0600

    Create problem2.cpp

---
+++ [2] Data Structure Lab (C++)/mid/assignment/problem2.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minKey = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[minKey] > arr[j])
+            {
+                minKey = j;
+            }
+        }
+        int tmp = arr[i];
+        arr[i] = arr[minKey];
+        arr[minKey] = tmp;
+    }
+}
+
+void binarySearch(int arr[], int n, int key)
+{
+    int low = 0;
+    int high = n - 1;
+
+    bool isFound = false;
+
+    cout << "Element: " << key << endl;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (arr[mid] == key)
+        {
+            cout << "Element Index: " << mid << endl;
+            cout << "Element Position: " << mid + 1;
+            isFound = true;
+            break;
+        }
+        else if (arr[mid] < key) low = mid + 1;
+        else high = mid - 1;
+    }
+    if (!isFound) cout << "Not found";
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    selectionSort(arr, n);
+
+    cout << "Sorted Array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    int key;
+    cin >> key;
+
+    binarySearch(arr, n, key);
+
+    return 0;
+}
