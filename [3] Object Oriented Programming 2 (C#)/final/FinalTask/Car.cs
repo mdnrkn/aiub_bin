3224255750c198b9f69951f32434fee0331a4a92
Md. Nishat Raihan Khan Nijhum <mdnishatraihankhan@gmail.com>
2025-05-23 23:58:41 +0600

    upload FinalTask

---
+++ [3] Object Oriented Programming 2 (C#)/final/FinalTask/Car.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalTask
+{
+    public class Car : ICar
+    {
+        private string carName;
+        private string model;
+        private string speed;
+        public string CarName
+        {
+            get { return carName; }
+            set { carName = value; }
+        }
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public string Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+
+        public Car()
+        { 
+        
+        }
+        public Car(string carName, string model, string speed)
+        {
+            this.carName = carName;
+            this.model = model;
+            this.speed = speed;
+        }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine("Car Name :" +this.carName);
+            Console.WriteLine("Car Model :" +this.model);
+            Console.WriteLine("Car Speed :" +this.speed);
+        }
+
+        public void Break()
+        {
+            Console.WriteLine("Car speed is decreasing");
+        }
+        public void Accelerate()
+        {
+            Console.WriteLine("Car speed is increasing");
+        }   
+    }
+}
---
+++ [3] Object Oriented Programming 2 (C#)/final/FinalTask/ICar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalTask
+{
+    public interface ICar
+    {
+        public abstract void Break();
+        public abstract void Accelerate();
+    }
+}
