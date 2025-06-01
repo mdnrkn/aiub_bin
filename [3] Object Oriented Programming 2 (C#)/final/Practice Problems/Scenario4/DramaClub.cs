a1de119c00bf06a79a91d9e544b360b9a85f440c
Md. Nishat Raihan Khan Nijhum <mdnishatraihankhan@gmail.com>
2025-06-01 06:13:14 +0600

    Create DramaClub.cs

---
+++ [3] Object Oriented Programming 2 (C#)/final/Practice Problems/Scenario4/DramaClub.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Term_Problem_Sheet.Scenario4
+{
+    public class DramaClub : IActivity
+    {
+        private string playTitle;
+        private string role;
+
+        public string PlayTitle
+        {
+            get { return this.playTitle; }
+            set { this.playTitle = value; }
+        }
+
+        public string Role
+        {
+            get { return this.role; }
+            set { this.role = value; }
+        }
+
+        public DramaClub()
+        {
+
+        }
+
+        public DramaClub(string playTitle, string role)
+        {
+            this.playTitle = playTitle;
+            this.role = role;
+        }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine("Play Title: " + this.playTitle);
+            Console.WriteLine("Role: " + this.role);
+        }
+
+        public void Participate()
+        {
+            Console.WriteLine("Acting as " + this.role + " in " + this.playTitle + ".");
+        }
+    }
+}
