95373348a0f3b7057a6cd26d64b97878b4bd304e
Md. Nishat Raihan Khan Nijhum <mdnishatraihankhan@gmail.com>
2025-06-01 06:13:03 +0600

    Create IPayable.cs

---
+++ [3] Object Oriented Programming 2 (C#)/final/Practice Problems/Scenario3/IPayable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Term_Problem_Sheet.Scenario3
+{
+    public interface IPayable
+    {
+        bool ProcessPayment(double amount);
+    }
+}
