dc7a008b52ae8097c5360fb14b30f1aaa4e01a5a
Md. Nishat Raihan Khan Nijhum <mdnishatraihankhan@gmail.com>
2025-06-01 06:13:33 +0600

    Create StudentReport.cs

---
+++ [3] Object Oriented Programming 2 (C#)/final/Practice Problems/Scenario5/StudentReport.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Term_Problem_Sheet.Scenario5
+{
+    public class StudentReport : Report
+    {
+        private string studentName;
+        private double averageScore;
+
+        public string StudentName
+        {
+            get { return this.studentName; }
+            set { this.studentName = value; }
+        }
+
+        public double AverageScore
+        {
+            get { return this.averageScore; }
+            set { this.averageScore = value; }
+        }
+
+        public StudentReport() : base()
+        {
+
+        }
+
+        public StudentReport(string reportTitle, DateTime createdDate, string studentName, double averageScore)
+            : base(reportTitle, createdDate)
+        {
+            this.studentName = studentName;
+            this.averageScore = averageScore;
+        }
+
+        public override string GenerateContent()
+        {
+            return "Student: " + this.studentName + ", Average Score: " + this.averageScore;
+        }
+
+        public override void PrintReport()
+        {
+            Console.WriteLine("Title: " + ReportTitle);
+            Console.WriteLine("Date: " + CreatedDate);
+            Console.WriteLine("Content: " + GenerateContent());
+        }
+
+        public void ShowDetails()
+        {
+            base.ShowDetails();
+            Console.WriteLine("Student Name: " + this.studentName);
+            Console.WriteLine("Average Score: " + this.averageScore);
+        }
+    }
+}
