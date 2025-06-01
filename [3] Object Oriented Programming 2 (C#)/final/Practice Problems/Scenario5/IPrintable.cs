2668754522979bfa37df3c1ee0d7c5b29def7b81
Md. Nishat Raihan Khan Nijhum <mdnishatraihankhan@gmail.com>
2025-06-01 06:13:26 +0600

    Create IPrintable.cs

---
+++ [3] Object Oriented Programming 2 (C#)/final/Practice Problems/Scenario5/IPrintable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Term_Problem_Sheet.Scenario5
+{
+    public interface IPrintable
+    {
+        void PrintReport();
+    }
+}
