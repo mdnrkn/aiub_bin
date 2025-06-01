f40286494956a4f6baafd215f411f74e544ac083
Md. Nishat Raihan Khan Nijhum <mdnishatraihankhan@gmail.com>
2025-06-01 06:13:23 +0600

    Create Sports.cs

---
+++ [3] Object Oriented Programming 2 (C#)/final/Practice Problems/Scenario4/Sports.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Term_Problem_Sheet.Scenario4
+{
+    public class Sports : IActivity
+    {
+        private string sportName;
+        private int teamSize;
+
+        public string SportName
+        {
+            get { return this.sportName; }
+            set { this.sportName = value; }
+        }
+
+        public int TeamSize
+        {
+            get { return this.teamSize; }
+            set { this.teamSize = value; }
+        }
+
+        public Sports()
+        {
+        }
+
+        public Sports(string sportName, int teamSize)
+        {
+            this.sportName = sportName;
+            this.teamSize = teamSize;
+        }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine("Sport Name: " + this.sportName);
+            Console.WriteLine("Team Size: " + this.teamSize);
+        }
+
+        public void Participate()
+        {
+            Console.WriteLine("Playing " + this.sportName + " in a team of " + this.teamSize + ".");
+        }
+    }
+}
