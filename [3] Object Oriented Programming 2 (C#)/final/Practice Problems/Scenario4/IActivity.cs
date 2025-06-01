68d5b068fbdb39437163ab41131c93b4cd0ef32e
Md. Nishat Raihan Khan Nijhum <mdnishatraihankhan@gmail.com>
2025-06-01 06:13:18 +0600

    Create IActivity.cs

---
+++ [3] Object Oriented Programming 2 (C#)/final/Practice Problems/Scenario4/IActivity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Term_Problem_Sheet.Scenario4
+{
+    public interface IActivity
+    {
+        void Participate();
+    }
+}
