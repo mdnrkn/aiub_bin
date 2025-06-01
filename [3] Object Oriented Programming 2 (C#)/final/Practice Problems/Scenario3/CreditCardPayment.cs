0a4204a6678e4b5b43a4b5c2acff5da9fc70709a
Md. Nishat Raihan Khan Nijhum <mdnishatraihankhan@gmail.com>
2025-06-01 06:12:58 +0600

    Create CreditCardPayment.cs

---
+++ [3] Object Oriented Programming 2 (C#)/final/Practice Problems/Scenario3/CreditCardPayment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Term_Problem_Sheet.Scenario3
+{
+    public class CreditCardPayment : Payment
+    {
+        public override bool Validate()
+        {
+            if (Amount < 1000)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public override bool ProcessPayment(double amount)
+        {
+            if (Validate())
+            {
+                Console.WriteLine("Payment successful.");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Payment failed.");
+                return false;
+            }
+        }
+    }
+}
